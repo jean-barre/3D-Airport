5ecabccf62e3e2aadc224877cb3f87e01ce15d04
jean <jean0barre@gmail.com>
2019-04-03 12:21:53 +0200

    ./: integrate an empty 3D scene in a QtQuick window.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <QQuickView>
+#include <QQmlContext>
+#include <QOpenGLContext>
 
-int main(int argc, char *argv[])
+void setSurfaceFormat()
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QSurfaceFormat format;
+#ifdef QT_OPENGL_ES_2
+    format.setRenderableType(QSurfaceFormat::OpenGLES);
+#else
+    if (QOpenGLContext::openGLModuleType() == QOpenGLContext::LibGL) {
+        format.setVersion(4, 3);
+        format.setProfile(QSurfaceFormat::CoreProfile);
+    }
+#endif
+    format.setDepthBufferSize(24);
+    format.setSamples(4);
+    format.setStencilBufferSize(8);
+    QSurfaceFormat::setDefaultFormat(format);
+}
 
+int main(int argc, char **argv)
+{
     QGuiApplication app(argc, argv);
+    setSurfaceFormat();
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
+    QQuickView view;
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.rootContext()->setContextProperty("_window", &view);
+    view.setSource(QUrl("qrc:/main.qml"));
+    view.setColor("#000000");
+    view.show();
 
     return app.exec();
 }
--- main.qml
+++ main.qml
@@ -1,9 +1,18 @@
-import QtQuick 2.9
+import QtQuick 2.0
 import QtQuick.Window 2.2
+import QtQuick.Scene3D 2.0
+import "qml"
 
-Window {
+Item {
+    id: mainview
+    width: 1280
+    height: 768
     visible: true
-    width: 640
-    height: 480
-    title: qsTr("Hello World")
+
+    Scene3D {
+        anchors.fill: parent
+        aspects: ["render", "logic", "input"]
+
+        AirportScene {}
+    }
 }
